921ef862536f8781c789b08d60650119a0388edd
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2017-05-16 21:40:41 +0200

    Temas de org-page

---
+++ org-page-config.el
@@ -0,0 +1,21 @@
+(require 'org-page)
+(setq op/repository-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io")
+(setq op/site-domain "https://alvarogonzalezsotillo.github.io/")
+;;; for commenting, you can choose either disqus, duoshuo or hashover
+(setq op/personal-disqus-shortname "your_disqus_shortname")
+(setq op/hashover-comments t)
+
+(setq op/personal-github-link "https://github.com/alvarogonzalezsotillo")
+
+
+
+(setq op/personal-avatar nil)
+(setq op/theme-root-directo nil)
+(setq op/them nil)
+
+
+(setq op/personal-avatar "octaedron.png" )
+(setq op/theme-root-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io/themes/")
+(setq op/theme 'mdo)
+;;; the configuration below are optional
+;(setq op/personal-google-analytics-id "your_google_analytics_id")
