babd02fd815b889f44cf7fd4beb6453ef7c6870d
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2017-08-29 16:17:13 +0200

    Probando gh-pages

--- org-page-config.el
+++ org-page-config.el
@@ -1,5 +1,7 @@
 (require 'org-page)
 (setq op/repository-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io")
+(setq op/repository-org-branch "source")
+(setq op/repository-html-branch "gh-pages")
 (setq op/site-domain "https://alvarogonzalezsotillo.github.io/")
 ;;; for commenting, you can choose either disqus, duoshuo or hashover
 (setq op/personal-disqus-shortname "your_disqus_shortname")
@@ -9,13 +11,12 @@
 
 
 
-(setq op/personal-avatar nil)
-(setq op/theme-root-directo nil)
-(setq op/them nil)
-
-
-(setq op/personal-avatar "/octaedron.png" )
+(setq op/personal-avatar "/assets/octaedron.png" )
 (setq op/theme-root-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io/themes/")
 (setq op/theme 'alvaro)
+
 ;;; the configuration below are optional
 ;(setq op/personal-google-analytics-id "your_google_analytics_id")
+
+(op/do-publication)
+(op/do-publication-and-preview-site op/site-preview-directory)
