5c64531811f1e2968490e88c93663ae9423c4a5c
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2017-10-17 11:42:58 +0200

    Borro todo para empezar otra vez

--- org-page-config.el
+++
@@ -1,8 +0,0 @@
-(require 'org-page)
-(setq op/repository-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io")
-(setq op/site-domain "https://alvarogonzalezsotillo.github.io/")
-;;; for commenting, you can choose either disqus, duoshuo or hashover
-(setq op/personal-disqus-shortname "your_disqus_shortname")
-(setq op/hashover-comments t)
-;;; the configuration below are optional
-;(setq op/personal-google-analytics-id "your_google_analytics_id")
