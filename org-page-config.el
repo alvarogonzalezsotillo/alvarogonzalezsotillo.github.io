1ad1777f553b111d99e01c40e7a4ed5d35d5ac4e
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2017-10-31 13:47:45 +0100

    genera-blog

--- org-page-config.el
+++ org-page-config.el
@@ -1,27 +1,30 @@
 (require 'org-page)
 
-(setq op/repository-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io")
-(setq op/repository-org-branch "source")
-(setq op/repository-html-branch "master")
-(setq op/site-domain "https://alvarogonzalezsotillo.github.io/")
-;;; for commenting, you can choose either disqus, duoshuo or hashover
-(setq op/personal-disqus-shortname "alvarogonzalezsotillo")
-(setq op/hashover-comments nil)
+(defun genera-blog ()
+  (interactive)
+  (setq op/repository-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io")
+  (setq op/repository-org-branch "source")
+  (setq op/repository-html-branch "master")
+  (setq op/site-domain "https://alvarogonzalezsotillo.github.io/")
+  ;;; for commenting, you can choose either disqus, duoshuo or hashover
+  (setq op/personal-disqus-shortname "alvarogonzalezsotillo")
+  (setq op/hashover-comments nil)
 
-(setq op/personal-github-link "https://github.com/alvarogonzalezsotillo")
+  (setq op/personal-github-link "https://github.com/alvarogonzalezsotillo")
 
 
 
-(setq op/personal-avatar "/media/img/octaedron.png" )
-(setq op/theme-root-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io/themes/")
-(setq op/theme 'alvaro)
+  (setq op/personal-avatar "/media/img/octaedron.png" )
+  (setq op/theme-root-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io/themes/")
+  (setq op/theme 'alvaro)
 
-(setq op/site-main-title "Álvaro González Sotillo")
-(setq op/site-sub-title "")
+  (setq op/site-main-title "Álvaro González Sotillo")
+  (setq op/site-sub-title "")
 
-;;; the configuration below are optional
-;(setq op/personal-google-analytics-id "your_google_analytics_id")
+  ;;; the configuration below are optional
+  ;(setq op/personal-google-analytics-id "your_google_analytics_id")
 
-;; TRAS LA CONFIGURACIÓN, GENERO EL SITIO DE PRUEBA
-; (op/do-publication-and-preview-site op/site-preview-directory)
-(op/do-publication t nil nil t nil)
+  ;; TRAS LA CONFIGURACIÓN, GENERO EL SITIO DE PRUEBA
+  ; (op/do-publication-and-preview-site op/site-preview-directory)
+  (op/do-publication t nil nil t nil))
+
