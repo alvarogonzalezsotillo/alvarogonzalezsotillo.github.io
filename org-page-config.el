5152e29017df94aecb1cee3c47cca4867a04491e
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2017-10-31 12:10:12 +0100

    vuelvo a source

--- org-page-config.el
+++ org-page-config.el
@@ -1,7 +1,7 @@
 (require 'org-page)
 
 (setq op/repository-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io")
-(setq op/repository-org-branch "draft")
+(setq op/repository-org-branch "source")
 (setq op/repository-html-branch "master")
 (setq op/site-domain "https://alvarogonzalezsotillo.github.io/")
 ;;; for commenting, you can choose either disqus, duoshuo or hashover
