806a530f5f1d546efeb3d4a2a26a1affd2f2c52f
Álvaro González Sotillo <alvaro@iesavellaneda.es>
2017-12-19 08:58:24 +0100

    generación como función interactiva

--- org-page-config.el
+++ org-page-config.el
@@ -1,25 +1,28 @@
-(require 'org-page)
-(setq op/repository-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io")
-(setq op/repository-org-branch "source")
-(setq op/repository-html-branch "master")
-(setq op/site-domain "https://alvarogonzalezsotillo.github.io/")
-;;; for commenting, you can choose either disqus, duoshuo or hashover
-(setq op/personal-disqus-shortname "alvarogonzalezsotillo")
-(setq op/hashover-comments nil)
 
-(setq op/personal-github-link "https://github.com/alvarogonzalezsotillo")
+(defun generar-blog()
+  (interactive)
 
+  (require 'org-page)
+  (setq op/repository-directory "/home/alvaro/alvarogonzalezsotillo.github.io")
+  (setq op/repository-org-branch "source")
+  (setq op/repository-html-branch "master")
+  (setq op/site-domain "https://alvarogonzalezsotillo.github.io/")
 
+  ;;; for commenting, you can choose either disqus, duoshuo or hashover
+  (setq op/personal-disqus-shortname "alvarogonzalezsotillo")
+  (setq op/hashover-comments nil)
 
-(setq op/personal-avatar "/media/img/octaedron.png" )
-(setq op/theme-root-directory "/home/alvaro/github/alvarogonzalezsotillo.github.io/themes/")
-(setq op/theme 'alvaro)
+  (setq op/personal-github-link "https://github.com/alvarogonzalezsotillo")
 
-(setq op/site-main-title "Álvaro González Sotillo")
-(setq op/site-sub-title "")
+  (setq op/personal-avatar "/media/img/octaedron.png" )
+  (setq op/theme-root-directory "/home/alvaro/alvarogonzalezsotillo.github.io/themes/")
+  (setq op/theme 'alvaro)
 
-;;; the configuration below are optional
-;(setq op/personal-google-analytics-id "your_google_analytics_id")
+  (setq op/site-main-title "Álvaro González Sotillo")
+  (setq op/site-sub-title "")
 
-;; TRAS LA CONFIGURACIÓN, GENERO EL SITIO DE PRUEBA
-(op/do-publication-and-preview-site op/site-preview-directory)
+  ;;; the configuration below are optional
+  ;(setq op/personal-google-analytics-id "your_google_analytics_id")
+
+  ;; TRAS LA CONFIGURACIÓN, GENERO EL SITIO DE PRUEBA
+  (op/do-publication-and-preview-site op/site-preview-directory))
