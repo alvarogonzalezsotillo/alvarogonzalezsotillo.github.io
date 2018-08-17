bae8a69a000a012c022462a1d099d54e33b271cf
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2018-08-17 20:10:39 +0200

    inicio de porcia con tabthis

---
+++ blog/como-se-llama-este-libro-porcia/tabthis.js
@@ -0,0 +1,58 @@
+function tabThis(elem,names){
+    const childs = elem.childNodes;
+    console.log(childs);
+    let c = 0;
+
+    const createDiv = function(clazz){
+        const ret = document.createElement("div");
+        ret.classList.add(clazz);
+        return ret;
+    };
+
+
+    const tabs = [];
+    childs.forEach( c => c.nodeType == 1 ? tabs.push(c) : undefined );
+
+    const tabButtonsBar = createDiv("tab-buttons-bar");
+    elem.append(tabButtonsBar);
+
+    const tabContents = createDiv("tab-contents");
+    elem.append(tabContents);
+
+    const tabButtonListener = function(button,tab){
+        return function(){
+            console.log(button);
+            tabs.forEach( t => t.classList.remove("tab-page-visible"));
+            tab.classList.add("tab-page-visible");
+        };
+    };
+
+    tabs[0].classList.add("tab-page-visible");
+    tabs.forEach( function(tab){
+        const name = names[c];
+        console.log( tab + " -- " + name );
+        tab.classList.add("tab-page");
+        tabContents.append(tab);
+
+        const button = createDiv("tab-button");
+        button.innerHTML = name;
+        tabButtonsBar.append(button);
+        button.onclick = tabButtonListener(button,tab);
+
+        
+        c += 1;
+    });
+
+    
+
+    elem.classList.add("tab-container");
+    
+}
+
+function tabElements(elementName){
+    const elements = document.getElementsByTagName(elementName);
+    for( let i = 0 ; i < elements.length ; i++ ){
+        const e = elements[i];
+        tabThis(e,["Enunciado","Implementación"]);
+    }
+}
