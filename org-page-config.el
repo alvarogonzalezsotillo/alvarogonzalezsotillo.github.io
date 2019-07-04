efbf2f1b10366ad235bb6cbbbf3cae6115ce9225
alvaro@lgq6 <alvaro@lgq6>
2019-07-04 08:58:18 +0200

    a

--- org-page-config.el
+++ org-page-config.el
@@ -3,7 +3,36 @@
 ;;; Code:
 
 (require 'org-page)
-(defvar raiz-de-blog "/home/alvaro/github/alvarogonzalezsotillo.github.io")
+(defvar raiz-de-blog "/data/data/com.termux/files/home/storage/external-1/alvarogonzalezsotillo.github.io")
+
+(defun enable-babel ()
+  (interactive)
+  (setq org-babel-load-languages '((scala . t) (shell . t) (emacs-lisp . t) (dot . t) (plantuml . t ) ( C . t)))
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '(
+     (C . t )
+     (dot . t)
+     (plantuml . t)
+     (scala . t)
+     (shell . t)))
+  )
+
+(defun disable-babel ()
+  (interactive)
+  (setq org-babel-load-languages '())
+
+  ;; (org-babel-do-load-languages
+  ;;  'org-babel-load-languages
+  ;;  '(
+  ;;    (C . t )
+  ;;    (dot . t)
+  ;;    (plantuml . t)
+  ;;    (scala . t)
+  ;;    (shell . t)))
+  ;; )
+)
 
 (defun configurar-blog()
   (interactive)
@@ -11,7 +40,7 @@
   (require 'org-page)
 
   (setq op/repository-directory raiz-de-blog)
-  (setq op/repository-org-branch "source")
+  (setq op/repository-org-branch "termux")
   (setq op/repository-html-branch "master")
   (setq op/site-domain "https://alvarogonzalezsotillo.github.io/")
 
