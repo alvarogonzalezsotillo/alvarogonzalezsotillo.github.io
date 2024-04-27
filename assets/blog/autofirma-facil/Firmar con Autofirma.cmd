fc24bdcc943d73958c102aa80466d30943a6df43
alvaro@thinkpad <alvaro@thinkpad>
2024-04-27 18:26:37 +0200

    Update published html files, committed by org-page.

---
+++ assets/blog/autofirma-facil/Firmar con Autofirma.cmd
@@ -0,0 +1,41 @@
+@echo off
+
+
+SET IMAGEN=/9j/4AAQSkZJRgABAQIAJQAlAAD/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD/2wBDAQMDAwQDBAgEBAgQCwkLEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBD/wAARCAAnAG8DAREAAhEBAxEB/8QAGgAAAgMBAQAAAAAAAAAAAAAAAAYFBwgECf/EADkQAAAFAgQEAgQPAQEAAAAAAAECAwQFBhEABxITCBQVIRYiFzFBdCMlMjM2UVNWYZOVs8PS0xhC/8QAGAEBAQEBAQAAAAAAAAAAAAAAAAECAwT/xAAbEQEBAQEBAQEBAAAAAAAAAAAAARECMSFBEv/aAAwDAQACEQMRAD8Axvj0POMAYAwBgDAGAMBNUrGNJJ45M+TFVJo3Ffb1CUDjrIQAEQ7289+31YLDJ02nvu2z/Ocf6YLg6bT33bZ/nOP9MDB02nvu2z/Ocf6YGIupYiLSigkmDIrQ6bgiBiEOcxTAcpxAfOIiAho+v24JYVcEGA0bAsMr0s48ucpXuSVLP2NQo0elISK8hMleqmkmLFVyoApviolNrcqCUASAoeUNI275+5rXzcae4QOEDh1zS4daSruu8vOpzknz/NOurPkdzbfOEieRJYpAsRMgdihe1x7iI4z11ZWueZYwNnRT8RSecVd0rT7TlYuGqWUj2KG4Y+03RdKETJqOImNYpQC5hERt3ERxueMX0m4qLTh16NpPJ2CqqSyvp6qJSZqWaj1V5d3Jk2m7VrGHSImVo7QL8p2sIiYDCNw7gAYn6v4aX+WUDNVzIpU3kDXVRMTQFMyoMaJfrAlGKvohBysVQy7V8oYp1VFBTAxwEAIYLn/8zVxWuadLeEahbxvo2rKid1mRfp9VL7rtW5zl3iDyrb4IdOkA2x8xD+YfUWxK4qH+flPcP50cUi2MsGlP8hW0/P0uwn+gU8m+aNHyzlNDfPJsW2o3LKpKDZNwrYNdriAiA2xK1FkUdTGWdZ0rGV29o2k4Zdn4u3IFJ1MiSZ5CGQdt7GFZQSbaqhjH+HR1EEALqHtiXZ8Uiyq1J1PlVM1JH5cwVOSUTUMSxTWi3MgfdQctpE6hTldOVi/KapCAlABCw9xvi/qKpqX6MK+/t/218VKSsGRgPVzJfhU8RZO0JUH/AEjnpGdTpqLecjHVhstGu41TPtIJ7I6Ei6tJS3GxQAL9scr198dZz89Yohsz/RPltQTHxbnJ8ewzqX5an8xOjsGnxq+bbaTbk1rX5bcMbX3MoYbBjebWNyKszoh/DucVd0/1WRk+mVLKM+ekV952623ShN1dSwa1TadRjWC5hEbd8WeJfSbio3zwC5C+lLJ2YqD0z5p0jy9SuGfI0rUfINFNLVqfdOntmuqO5pE1+5SEC3bHPu5XTibFF8SVAZfUbxFVvSVd5oZhPOm9M5WTcsEJ1+83GKKh+ZWVdNbaNRCksBvKAANtIatS2xnqSVnrGmTLQ/z8p7h/OjgsOdN1bVdGPlJSj6mloJ4qkLc7iNeqNVTpCIGEgmTMAiURKUbXtcoD7MM1ppnIZvVGfb/LyPqXN2dLJRlQ1JzL5KrE0p5q2UjGQocsC5zLmTMokuUdshgAu7fT3HGL8an018beSno4yqipz0tZk1Rv1Cg05OpJ7nWyeps5NulT2y2UDRpA1/UYwe3E4u06mMMVL9GFff2/7a+OjFJWDIwF6U5xv8UNJ09F0rT+Z3KxcMzQj2KHRY4+03RIBEyajoCY1ilALmERG3cRHGf5jX9UixOdNcw8DGU2kjSz5jDIqNmIStIREiqgkdZRcyYLOWyimndWVPpEwgAnG1sXE0rVHUEvVlQylVVA75qUmXi8g+X2yk3XCxxOofSQAKW5jCNigABfsABiojsBaeVnE/nnkpTzilcsq46NFunh5BZDpjNxqcGIQhj6lkjmC5UiBYBt29VxG8vMvqzqzxHSfELnfJ1dNV36Uahj5youW6q6iHho3m+XSBJHcI12yDoIFg8vtMPrERFkNqvcVDLQ/wA/Ke4fzo4LE9g0naHriqMuKoZVpRcp06ZjtzlnOwmtt7iZkz+RQpiDchzB3AfXcO9hws08NeZPEXnJm7BoU3mHWPVo1s7K+SR6e1Q0rlIcgG1IpFMPlUOFhG3f1dgxJzJ4W2qpqX6MK+/t/wBtbFSkrBkYAwBgDAGAMAYAwHQwkHkY4B0xWFJQAEL2AQEB9YCA9hD8BwEp41qH7Zn+nt/6YLo8a1D9sz/T2/8ATA0eNah+2Z/p7f8Apga4pKdlJcpCPnBTETERKRNIiZQH69JAAL/jgjgwH//Z
+echo Los ficheros a firmar son %*
+
+:FIRMASIGUIENTE
+
+if "%~1"=="" (
+    echo No quedan más ficheros que firmar
+    goto :end
+)
+
+set DNI=1234
+set ARCHIVO=%~dpF1
+set ARCHIVOSINEXTENSION=%~dpn1
+
+echo Se va a firmar el archivo %ARCHIVO%
+
+SET PositionOnPageLowerLeftX=50  
+SET PositionOnPageLowerLeftY=135   
+SET PositionOnPageUpperRightX=250 
+SET PositionOnPageUpperRightY=245 
+SET l2FontColor=black             
+SET l2FontSize=7                  
+SET l2FontFamily=1                
+SET l2FontStyle=0                 
+SET LAYER2TEXT=Firmado por $$GIVENNAME$$ $$SURNAME$$ el $$SIGNDATE=dd/MM/YYYY$$ con un certificado emitido por $$ISSUERCN$$ con n£mero de serie $$CERTSERIAL$$
+
+
+SET signaturePage=1               
+SET CONFIG=headless=true\n layer2Text=%LAYER2TEXT%\n    signaturePositionOnPageLowerLeftX=%PositionOnPageLowerLeftX%\n    signaturePositionOnPageLowerLeftY=%PositionOnPageLowerLeftY%\n    signaturePositionOnPageUpperRightX=%PositionOnPageUpperRightX%\n    signaturePositionOnPageUpperRightY=%PositionOnPageUpperRightY%\n    layer2FontColor=%l2FontColor%\n    layer2FontSize=%l2FontSize%\n    layer2FontFamily=%l2FontFamily%\n    layer2FontStyle=%l2FontStyle%\n       signaturePage=%signaturePage% \n signatureRubricImage=%IMAGEN%
+
+echo   Primera firma en p gina 1
+autofirmacommandline sign -i "%ARCHIVO%" -o "%ARCHIVOSINEXTENSION% - firmado.pdf" -config "%CONFIG%" -filter subject.contains:%DNI%;nonexpired:
+
+shift
+goto :FIRMASIGUIENTE
+
+
+:end
